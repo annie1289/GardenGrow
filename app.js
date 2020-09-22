107528629390e50d0cfc3df600dc9b86f20a9c20
Annie Wang <anniewang@annies-mbp-2.lan>
2020-09-22 10:08:52 -0400

    pseudocode

--- app.js
+++ app.js
@@ -12,10 +12,24 @@ const getData = async () => {
 }
 getData()
 
-// const fetch = require('node-fetch');
 
-// (async () => {
-//   const response = await fetch('https://trefle.io/api/v1/plants?token=XnHXzqWAsibRqMprXaZJGxbOw_iIMqO2z9ZyvePxlHE');
-//   const json = await response.json();
-//   console.log(json);
-// })();
+//select input form and button
+const input = document.querySelector("#search-plant")
+const button = document.querySelector("#button")
+
+//create event listener for when you click the button, so that we can use the aync/await to get the plant data from the API
+
+//create a div for the plants
+
+//may need to limit the number of plants displayed
+
+//create an element for the plant common-name and append it to the div
+
+//create an element for the plant photo and append it to the div
+
+//create an element for the plant scientific name and append it to the dom
+
+//create function to remove a plant when you search for a new movie
+
+//create a line that makes it so if a plant does not have an image it will not be displayed
+
