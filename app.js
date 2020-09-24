2a47041444ba64a63516f817fb948cdd52d75495
Annie Wang <anniewang@annies-mbp-2.lan>
2020-09-24 00:36:50 -0400

    update

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const getData = async () => {
-  
-  const url = "https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants?token=XnHXzqWAsibRqMprXaZJGxbOw_iIMqO2z9ZyvePxlHE"
-
-  try {
-    const response = await axios.get(url)
-    console.log(response.data)
-
-  } catch (error) {
-    console.log(`Error: ${error}`)
-  }
-}
-getData()
-
-
 //select input form and button
 const input = document.querySelector("#search-plant")
 const button = document.querySelector("#button")
+const randombutton = document.querySelector("#button2")
 
 //create event listener for when you click the button, so that we can use the aync/await to get the plant data from the API
+button.addEventListener('click', async () => {
+  let value = input.value
+  const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants/search?token=XnHXzqWAsibRqMprXaZJGxbOw_iIMqO2z9ZyvePxlHE&q=${value}`, {"headers": {"Access-Control-Allow-Origin": "*"}})
+  console.log(response.data)
+  const list = Object.keys(response.data)
+  findAll(list)
+})
 
-//create a div for the plants
+//plants section
+const plantDisplay = document.querySelector(".plants")
 
-//may need to limit the number of plants displayed
-
-//create an element for the plant common-name and append it to the div
-
-//create an element for the plant photo and append it to the div
-
-//create an element for the plant scientific name and append it to the dom
-
+const findAll = plants => {
+  plants.forEach(plant => {
+  const plantContainer = document.querySelector('plant-container')
+  const comName = document.querySelector('.plantcommonname')
+  const sciName = document.querySelector('.scientificname')
+  comName.innerHTML = `${plant.common_name}`
+  sciName.innerHTML = `${plant.scientific_name}`
+  const image = document.createElement('img')
+  image.src = plant.image_url
+  plantContainer.appendChild(image)
+  plantDisplay.appendChild(plantContainer)
+  document.querySelector('#search-plant').value =''
+  })
+}
 //create function to remove a plant when you search for a new movie
 
 //create a line that makes it so if a plant does not have an image it will not be displayed
