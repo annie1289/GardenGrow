246f3b97b9874cdc51b2d286c13e59e524790e64
Annie Wang <anniewang@annies-mbp-2.lan>
2020-09-25 10:14:59 -0400

    random gen

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
-//select input form and button
 const input = document.querySelector("#search-plant")
 const button = document.querySelector("#button")
-
-//create event listener for when you click the button, so that we can use the aync/await to get the plant data from the API
+//search button
 button.addEventListener('click', async () => {
   let value = input.value
   const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants/search?token=XnHXzqWAsibRqMprXaZJGxbOw_iIMqO2z9ZyvePxlHE&q=${value}`, {"headers": {"Access-Control-Allow-Origin": "*"}})
   console.log(response.data.data)
   findAll(response.data.data)
 })
+//random button
+button2.addEventListener('click', async () => {
+  let page = Math.ceil((Math.random() * 1000))
+  console.log(page)
+  const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants?token=XnHXzqWAsibRqMprXaZJGxbOw_iIMqO2z9ZyvePxlHE&page=${page}`, {"headers": {"Access-Control-Allow-Origin": "*"}})
+  console.log(response.data.data)
+  findAll(response.data.data)
+})
 
 const plantDisplay = document.querySelector(".plants")
-
+//finds common name, scientific name, family, and a photo of the search results
 const findAll = plants => {
   removePlants()
   plants.forEach(plant => {
     const plantContainer = document.createElement('div')
     plantContainer.className = "plant-container"
-    if (plant.common_name !== null && plantContainer.scientific_name !==null && plant.image_url !== null) {
+    if (plant.common_name !== null && plant.family !==null && plant.scientific_name !==null && plant.image_url !== null) {
     const comName = document.createElement('h3')
     comName.innerHTML = `${plant.common_name}`
     plantContainer.appendChild(comName)
     const sciName = document.createElement('p')
-    sciName.innerHTML = `${plant.scientific_name}`
+    sciName.innerHTML = `Scientific Name: ${plant.scientific_name}`
     plantContainer.appendChild(sciName)
+    const family = document.createElement('p')
+    family.innerHTML = `Family: ${plant.family}`
+    plantContainer.appendChild(family)
     const image = document.createElement('img')
     image.src = plant.image_url
     plantContainer.appendChild(image)
     }
-    
+
     plantDisplay.appendChild(plantContainer);
     document.querySelector('#search-plant').value = ''
   })
